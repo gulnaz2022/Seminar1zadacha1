d8368651dc05d7cef0d54ecbca0c8af0742382e1
Gulnaz <gulism86@gmail.com>
2023-01-07 01:33:24 +0400

    решение второй задачи

---
+++ seminar1zadacha2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число1:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число2:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число3:");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num1 > max) max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+Console.Write("максимальное - ");
+Console.Write(max);
+
+
