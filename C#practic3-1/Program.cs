2f32e0a24007f4662febf980bd44972e6e57cfaa
Gulnaz <gulism86@gmail.com>
2023-01-12 15:31:27 +0400

    решение 1 задачи по семинару 3

---
+++ C#practic3-1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+
+Console.WriteLine("Введите 5-значное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int poz1 = num/10000;
+int poz2 = (num/1000)%10;
+int poz4 = (num/10)%10;
+int poz5 = num%10;
+if (poz1==poz5&&poz2==poz4)
+{
+    Console.WriteLine("Полиндром");
+}
+else
+{
+    Console.WriteLine("Не полиндром");
+}
