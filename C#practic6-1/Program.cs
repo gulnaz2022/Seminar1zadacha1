a771ed152d858f8d6ad4535675d2f99afb17bc20
Gulnaz <gulism86@gmail.com>
2023-01-21 12:05:44 +0400

    решение задачи 1 по семинару 6

---
+++ C#practic6-1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+int[] creatArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Введите число");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+int pozSum(int[] array)
+{
+int count = 0;
+foreach (int a in array){
+    if (a > 0) count++;
+}return count;
+}    
+int m = new Random().Next(1, 10);
+Console.WriteLine($"Ввепдите {m} положительных ит отрицательных чисел");
+int[] numbers = new int[m];
+Console.WriteLine(creatArray(numbers));
+Console.WriteLine($"В массиве {pozSum(numbers)} положительных чисел");
+
